55046bb126e4815c4fd3153b3d85d9004b567b55
Tomasz <dworowytomasz@gmail.com>
2020-02-01 11:31:23 +0100

    update

--- api/api.py
+++ api/api.py
@@ -1,14 +1,36 @@
+import json
+
 from flask import Flask, jsonify, request
 
 from wrapper.keras_wrapper import KerasWrapper, ModelBuilder, DenseLayerBuilder
 
 
+def get_model(values: json):
+    model = ModelBuilder().model()
+    for _layer in values['layers']:
+        layer = DenseLayerBuilder(). \
+            units(_layer['units']). \
+            activation(_layer['activation'])
+        if "input_shape" in _layer:
+            layer = layer.input_shape(_layer['input_shape'])
+
+        model = model.layer(layer.build())
+    return model
+
+
+app = Flask(__name__)
+
+
 class API:
+
     @staticmethod
-    def start(host, port):
-        app = Flask(__name__)
-        keras_wrapper = KerasWrapper()
+    def start(app, host, port):
         app.run(host=host, port=port)
+
+    @staticmethod
+    def new_api():
+        keras_wrapper = KerasWrapper()
+        app = Flask(__name__)
 
         @app.route('/network/new', methods=['POST'])
         def new_transaction():
@@ -17,19 +39,22 @@ class API:
             if not all(k in values for k in required):
                 return 'Missing values', 400
             else:
-                model = ModelBuilder().model()
-                for _layer in values['layers']:
-                    layer = DenseLayerBuilder().\
-                        units(_layer['units']).\
-                        activation(_layer['activation'])
 
-                    if "input_shape" in _layer:
-                        layer = layer.input_shape(_layer['input_shape'])
+                keras_wrapper.add_model(values['name'], get_model(values).build())
+                response = {
+                    "Message": "New Network added."
+                }
+                return jsonify(response), 200
 
-                    model = model.layer(layer.build())
+        @app.route('/', methods=['GET'])
+        def default():
+            response = {
+                "Message": "API running"
+            }
+            return jsonify(response), 200
 
-                keras_wrapper.add_model(values['name'], model.build())
+        return app
 
 
 if __name__ == '__main__':
-    API.start(host='0.0.0.0', port=5000)
+    API.start(API.new_api(), host='0.0.0.0', port=5000)

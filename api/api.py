26576d5eaac1c7d09acd7bc928677d626e5dd641
Tomasz <dworowytomasz@gmail.com>
2020-02-08 13:37:50 +0100

    update

--- api/api.py
+++ api/api.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from data.data_provider import get_keras_data_set
 from wrapper.keras_wrapper import KerasWrapper, ModelBuilder, DenseLayerBuilder
 
@@ -19,6 +19,14 @@ def build_model(values: json):
     return model.build()
 
 
+def prepare_response(message: json, method: str, status: int):
+    response = make_response(jsonify(message), status)
+    response.headers.extend({"Access-Control-Allow-Origin": "*"})
+    response.headers.extend({"Access-Control-Allow-Methods": method})
+    response.headers.extend({"Access-Control-Allow-Headers", "accept, content-type"})
+    return response
+
+
 app = Flask(__name__)
 
 
@@ -27,14 +35,14 @@ def new_network():
     values = request.get_json()
     required = ['name', 'layers']
     if not all(k in values for k in required):
-        return 'Missing values', 400
+        return prepare_response('Missing values', 'POST, OPTIONS', 400)
     else:
 
         keras_wrapper.add_model(values['name'], build_model(values))
         response = {
             "Message": f"New Network {values['name']} added."
         }
-        return jsonify(response), 200
+        return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
 
 @app.route('/network/compile', methods=['POST'])
@@ -42,13 +50,13 @@ def compile_network():
     values = request.get_json()
     required = ['name', 'optimizer', 'loss', 'metrics']
     if not all(k in values for k in required):
-        return 'Missing values', 400
+        prepare_response(jsonify({"Message": "Missing value"}), 'POST, OPTIONS', 400)
     else:
         if not keras_wrapper.models.get(values["name"], None):
             response = {
                 "Message": f"Network {values['name']} not found."
             }
-            return jsonify(response), 200
+            return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
         keras_wrapper.compile(model_name=values['name'],
                               optimizer=values['optimizer'],
@@ -57,7 +65,7 @@ def compile_network():
         response = {
             "Message": f"Network {values['name']} compiled."
         }
-        return jsonify(response), 200
+        return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
 
 @app.route('/network/train', methods=['POST'])
@@ -65,20 +73,20 @@ def train_network():
     values = request.get_json()
     required = ['name', 'data_set', 'epochs', 'batch_size']
     if not all(k in values for k in required):
-        return 'Missing values', 400
+        return prepare_response(jsonify({"Message": "Missing value"}), 'POST, OPTIONS', 400)
     else:
 
         if not keras_wrapper.models.get(values["name"], None):
             response = {
                 "Message": f"Network {values['name']} not found."
             }
-            return jsonify(response), 200
+            return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
         if not keras_wrapper.models.get(values["name"], None).compiled:
             response = {
                 "Message": f"Network {values['name']} need to be compiled."
             }
-            return jsonify(response), 200
+            return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
         (train_data, train_labels), (val_data, val_labels) = get_keras_data_set(values["data_set"],
                                                                                 int(values['input_shape']))
@@ -92,7 +100,7 @@ def train_network():
         response = {
             "Message": f"Network {values['name']} training complete."
         }
-        return jsonify(response), 200
+        return prepare_response(jsonify(response), 'POST, OPTIONS', 200)
 
 
 @app.route('/network/delete', methods=['DELETE'])
@@ -106,12 +114,12 @@ def delete_network():
             response = {
                 "Message": f"Network {values['name']} not found."
             }
-            return jsonify(response), 200
+            return prepare_response(jsonify(response), 'DELETE, OPTIONS', 200)
         keras_wrapper.models.pop(values['name'])
         response = {
             "Message": f"Network {values['name']} deleted."
         }
-        return jsonify(response), 200
+        return prepare_response(jsonify(response), 'DELETE, OPTIONS', 200)
 
 
 @app.route('/', methods=['GET'])
@@ -119,7 +127,7 @@ def default():
     response = {
         "Message": "API running"
     }
-    return jsonify(response), 200
+    return prepare_response(jsonify(response), 'GET', 200)
 
 
 @app.route('/networks', methods=['GET'])
@@ -127,7 +135,7 @@ def get_networks():
     response = {
         keras_wrapper.models.keys()
     }
-    return jsonify(response), 200
+    return prepare_response(jsonify(response), 'GET', 200)
 
 
 @app.route('/data-sources', methods=['GET'])
@@ -135,7 +143,7 @@ def get_data_sources():
     response = {
         get_data_sources()
     }
-    return jsonify(response), 200
+    return prepare_response(jsonify(response), 'GET', 200)
 
 
 if __name__ == '__main__':
--- dashboard/dashboard/src/NewNetworkForm.tsx
+++ dashboard/dashboard/src/NewNetworkForm.tsx
@@ -11,59 +11,77 @@ class Layer {
         this.activation = activation
         this.input_shape = input_shape
     }
-
+}
+async  function postNetwork(data:any) {
+    const response = await fetch('http://localhost:5000/network/new', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+        })
+    return await response.json()
 }
 
 type State = { name: string, layers:Array<any>};
 export class NewNetwork extends React.Component<{},State> {
     constructor(props:any) {
       super(props);
+      this.addLayerHandler = this.addLayerHandler.bind(this);
+      this.addNetworkHandler = this.addNetworkHandler.bind(this);
       this.state = { 
           name: '', 
           layers:[]
         }
     }
     addNetworkHandler = (event:any) => {
-      "pass"
+        console.log(`POST:${this.state}`);
+        postNetwork(this.state)
+            .then((data) => {
+                console.log(data);
+            });
+        
     }
     addLayerHandler = (event:any) => {
+        console.log(`Add layer:${event.target}`);
         let layer:Layer = new Layer(event.target.units,
                           event.target.activation,
                           event.target.input_shape)
         this.setState(
-            {name: event.target.name,
-            layers:  [...this.state.layers, layer]  
+            { name: event.target.name,
+              layers:  [...this.state.layers, layer]  
             });
       }
     render() {
       return (
-        <form>
-        <p>Network name:</p>
-        <input
-          name = 'name'
-          type = 'text'
-          defaultValue={this.state.name}
-        />
-        <p>units:</p>
-        <input
-          name = 'units'
-          type = 'text'
-        />
-        <p>activation:</p>
-        <input
-          name = 'activation'
-          type ='text'
-        />
-        <p>input_shape:</p>
-        <input
-          name = 'input_shape'
-          type ='text'
-        />
-        <br></br>
-        <input type="submit" value="Add network"  onSubmit={this.addNetworkHandler}/>
-        <input type="submit" value="Add layer"  onSubmit={this.addLayerHandler}/>
-        </form>
-        
-      );
+            <div>
+            <form>
+            <p>Network name:</p>
+            <input
+            name = 'name'
+            type = 'text'
+            defaultValue={this.state.name}
+            />
+            <p>units:</p>
+            <input
+            name = 'units'
+            type = 'text'
+            />
+            <p>activation:</p>
+            <input
+            name = 'activation'
+            type ='text'
+            />
+            <p>input_shape:</p>
+            <input
+            name = 'input_shape'
+            type ='text'
+            />
+            <br></br>
+            <input type="submit" value="Add layer" onChange={this.addLayerHandler}/>
+            </form>
+            <button onClick ={this.addNetworkHandler}>Add new network</button> 
+            </div>
+        );
     }
   }

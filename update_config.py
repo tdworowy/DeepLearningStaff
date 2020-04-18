a7e4daa740ce90abaf4190a64728fadb44d329ca
Tomasz <dworowytomasz@gmail.com>
2020-04-18 16:25:10 +0200

    update

--- update_config.py
+++ update_config.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+
 if __name__ == "__main__":
     fname = "config.yaml"
 
@@ -8,8 +9,8 @@ if __name__ == "__main__":
     stream = open(fname, 'r')
 
     data = yaml.load(stream)
-    data["mongo_host"] = ip
-    data["nats_host"] = ip
+    data["mongo_host"] = f'"{ip}"'
+    data["nats_host"] = f'"{ip}"'
 
     with open(fname, 'w') as yaml_file:
         yaml_file.write(yaml.dump(data, default_flow_style=False))

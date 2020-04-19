66035d22ccf3c754983005b749872bbd964b3a87
Tomasz <dworowytomasz@gmail.com>
2020-04-19 20:29:51 +0200

    update

--- monitor_containers_logs.py
+++ monitor_containers_logs.py
@@ -2,5 +2,5 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
-    for container_name in sys.argv[1].split("\n"):
+    for container_name in sys.argv[1].split(","):
         subprocess.Popen([f"docker logs -f {container_name} > {container_name}.log"], shell=True)

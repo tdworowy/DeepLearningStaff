9bf05a1a1ff185e9afeca7d9de817f91eaf00ba6
Tomasz <dworowytomasz@gmail.com>
2020-04-20 17:39:18 +0200

    update

--- tests/front_end_tests/webdriver_wapper/webdrover_factory.py
+++ tests/front_end_tests/webdriver_wapper/webdrover_factory.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+import platform
+
+systems = {"Windows": "_win.exe", "Linux": "_lin"}
 
 
 def chromedriver(executable_path: str) -> webdriver.Chrome:
+    system = platform.system()
+    executable_path = executable_path + systems[system]
     DesiredCapabilities.CHROME['goog:loggingPrefs'] = {'browser': 'ALL'}
     return webdriver.Chrome(executable_path=executable_path)
 

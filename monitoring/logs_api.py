588914503c1a7e690d84150eb58b01994f9b7cb9
Tomasz <dworowytomasz@gmail.com>
2020-05-23 15:09:05 +0200

    fix

--- monitoring/logs_api.py
+++ monitoring/logs_api.py
@@ -15,7 +15,7 @@ logs_path = path.join(path.dirname(path.realpath(__file__)), "../logs/logs.log")
 def get_logs():
     try:
         with open(logs_path) as log_file:
-            return [line for line in log_file.readlines()[-1000:]]
+            return log_file.readlines()[-1000:]
     except FileNotFoundError as ex:
         return f"No logs available: {ex} "
 
@@ -51,7 +51,7 @@ def read_config():
 
 if __name__ == '__main__':
     config = read_config()
-    if  len(sys.argv) ==1:
+    if len(sys.argv) == 1:
         port = config.get('log_api_port')
     else:
         port = sys.argv[1]
